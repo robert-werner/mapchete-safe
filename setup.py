3aba987a4a4c422beb7afb671b00ad7f7b14aa8d
Joachim Ungar <joachim.ungar@eox.at>
2017-09-22 16:21:21 +0200

    updating for mapchete 0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='mapchete-safe',
-    version='0.2',
+    version='0.3',
     description='Mapchete SAFE file read extension',
     author='Joachim Ungar',
     author_email='joachim.ungar@eox.at',
@@ -12,7 +12,7 @@ setup(
     license='MIT',
     packages=['mapchete_safe'],
     install_requires=[
-        'mapchete>=0.7',
+        'mapchete>=0.8',
         's2reader>=0.4',
         'cached_property'
         ],
